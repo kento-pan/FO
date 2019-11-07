f52e438d0d337655db0dbc5b87720ba51f7af355
kento-pan <56069650+kento-pan@users.noreply.github.com>
2019-11-07 14:02:33 +0100

    Restructered and cleaned the code a little

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ print("### FO - File Organizer ###")
 print("Press CTRL + C to stop File Organizer.")
 
 # Creating folders in user/pictures/
-path_to_fo = Path.home() / "Pictures/FO - File Organizer/"
-path_to_src_files = str(path_to_fo) + "/Move your files in here/"
-path_to_files_output = str(path_to_fo) + "/Organized files/"
+path_to_fo = Path.home() / "Pictures" / "FO - File Organizer"
+path_to_src_files = path_to_fo / "Move your files in here"
+path_to_files_output = path_to_fo / "Organized files"
 
-if not os.path.isdir(str(path_to_fo)):
-    os.mkdir(str(path_to_fo))
-if not os.path.isdir(str(path_to_src_files)):
-    os.mkdir(str(path_to_src_files))
-if not os.path.isdir(str(path_to_files_output)):
-    os.mkdir(str(path_to_files_output))
+if not os.path.isdir(path_to_fo):
+    os.mkdir(path_to_fo)
+if not os.path.isdir(path_to_src_files):
+    os.mkdir(path_to_src_files)
+if not os.path.isdir(path_to_files_output):
+    os.mkdir(path_to_files_output)
 
 print(f"Place your files into the folder {path_to_src_files}.")
 print(f"They will be moved to {path_to_files_output}.")
@@ -25,29 +25,32 @@ print(f"They will be moved to {path_to_files_output}.")
 while True:
     try:
         # Checking for file in source folder
-        for file in os.listdir(str(path_to_src_files)):
+        for file in os.listdir(path_to_src_files):
+            # File paths
+            pathjoin_src = os.path.join(path_to_src_files, file)
+            pathjoin_copy = os.path.join(path_to_src_files, "duplicate-" + file)
             # Getting and converting the Last Modified time of file
-            time_lm = os.path.getmtime(str(path_to_src_files) + "/" + str(file))
+            time_lm = os.path.getmtime(pathjoin_src)
             time_lm_conv = datetime.fromtimestamp(time_lm).strftime("%Y-%m")
-            path_to_date_folder = str(path_to_files_output) + str(time_lm_conv)
+            path_to_date_folder = path_to_files_output / time_lm_conv
             # Creating folder by month
-            if not os.path.isdir(str(path_to_files_output) + str(time_lm_conv)):
+            if not os.path.isdir(path_to_files_output / time_lm_conv):
                 os.mkdir(path_to_date_folder)
             # Check if file exists already
-            if os.path.isfile(path_to_date_folder + "/" + str(file)) is False:
+            if os.path.isfile(path_to_date_folder / file):
+                # Check for duplicates and rename file if necessary
                 try:
-                    # Move file
-                    shutil.move(str(path_to_src_files) + "/" + str(file), path_to_date_folder)
-                    print(f"File {file} moved to {path_to_date_folder}")
+                    # Rename file
+                    os.rename(pathjoin_src, pathjoin_copy)
+                    print(f"{file} exists already. Renamed to duplicate-{file} and moved.")
                 except PermissionError:
                     print("Permission error.")
                     break
-            # Check for duplicates and rename file if necessary
             else:
                 try:
-                    # Rename file
-                    os.rename(str(path_to_src_files) + str(file), str(path_to_src_files) + "duplicate-" + str(file))
-                    print(f"{file} exists already. Renamed to duplicate-{file} and moved.")
+                    # Move file
+                    shutil.move(pathjoin_src, path_to_date_folder)
+                    print(f"File {file} moved to {path_to_date_folder}")
                 except PermissionError:
                     print("Permission error.")
                     break
